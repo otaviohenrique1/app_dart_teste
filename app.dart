5ad4f97974f20c580a68771150624cc863dc5fc3
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-01-01 12:25:01 -0300

    asfasfasd

--- app.dart
+++ app.dart
@@ -5,6 +5,17 @@ class Calculadora {
 }
 
 main() {
-  Calculadora x = new Calculadora();
-  print(x.soma(1, 2));
+  // Calculadora x = new Calculadora();
+  // print(x.soma(1, 2));
+  Map<dynamic, dynamic> lista1 = {
+    'nome': 'Pombo',
+    'sobrenome': 'Cavalo',
+    'idade': '100',
+  };
+
+  Map<dynamic, dynamic> lista2 = lista1;
+
+  for (var item in lista2.keys) {
+    print(lista2[item]);
+  }
 }
