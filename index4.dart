21a317e676d12e90d9d0f5880071027c96a2f556
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-04-06 23:22:15 -0300

    asdasdasd

--- index4.dart
+++ index4.dart
@@ -1,25 +1,27 @@
-// String arrumaTexto(String texto) {
-//   return texto.toLowerCase().replaceAll('-', '_');
-// }
+// import 'calculadora.dart';
 
-import 'calculadora.dart';
+String arrumaTexto(String texto) {
+  return texto
+      .toLowerCase()
+      .replaceAll(new RegExp(r'/\s+/g'), '-')
+      .replaceAll(new RegExp(r'/\.+/g'), '-');
+}
 
 main(List<String> args) {
   // num x = 2;
   // print(1 + int.parse('1') + x);
-  // print(arrumaTexto('Ford-V3000S_2'));
-  Map<dynamic, dynamic> x =
-      Calculadora.teoremaDePitagoras3(cateto1: 3, cateto2: 4);
-  for (var item in x.keys) {
-    print('$item -> ${x[item]}');
-  }
-  for (var item in x.values) {
-    print('$item');
-  }
-  for (var item in x.entries) {
-    print('$item');
-  }
-
-  List<dynamic> y = Calculadora.teoremaDePitagoras4(cateto1: 3, cateto2: 4);
-  print('${y[0]} - ${y[1]}');
+  // Map<dynamic, dynamic> x =
+  //     Calculadora.teoremaDePitagoras3(cateto1: 3, cateto2: 4);
+  // for (var item in x.keys) {
+  //   print('$item -> ${x[item]}');
+  // }
+  // for (var item in x.values) {
+  //   print('$item');
+  // }
+  // for (var item in x.entries) {
+  //   print('$item');
+  // }
+  // List<dynamic> y = Calculadora.teoremaDePitagoras4(cateto1: 3, cateto2: 4);
+  // print('${y[0]} - ${y[1]}');
+  // print(arrumaTexto('Ford V3000S.2'));
 }
